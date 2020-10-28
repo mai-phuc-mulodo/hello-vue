0806c355a37f3316e8b28ac7dbf458dce7605a19
mai-phuc-mulodo <mai.phuc@mulodo.com>
2020-10-28 09:31:01 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hello-vue
+
+## Getting Started
+
+    $ git clone https://github.com/mai-phuc-mulodo/hello-vue.git
+    $ cd hello-vue
+    $ npm install
+    $ npm run dev
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="row text-center">
+        <div class="col-md-6 offset-md-3">
+            <div id="form-wrapper">
+                <form action="/" method="get">
+                    <h3>Todo App</h3>
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Content">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="submit">Add</button>
+                        </div>
+                    </div>
+
+                    <ul class="list-group">
+                        <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <span class="todo-content">Buy eggs</span>
+                            </div>
+                            <span class="badge badge-danger">x</span>
+                        </li>
+                        <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center todo-completed">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <span class="todo-content">Buy milk</span>
+                            </div>
+                            <span class="badge badge-danger">x</span>
+                        </li>
+                        <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox">
+                                <span class="todo-content">Buy vegetable</span>
+                            </div>
+                            <span class="badge badge-danger">x</span>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
